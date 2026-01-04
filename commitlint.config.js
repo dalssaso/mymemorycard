45e7060b56d3afd9a81a5ae4ce28ef9f8a920aab
Henrique Dalssaso <henrique@dalssaso.com.br>
2026-01-04 02:30:01 -0300

    chore: add editorconfig and commitlint

---
+++ commitlint.config.js
@@ -0,0 +1,12 @@
+module.exports = {
+  extends: ['@commitlint/config-conventional'],
+  rules: {
+    'type-enum': [
+      2,
+      'always',
+      ['feat', 'fix', 'refactor', 'test', 'docs', 'chore', 'perf'],
+    ],
+    'subject-case': [2, 'always', 'lower-case'],
+    'header-max-length': [2, 'always', 100],
+  },
+}
