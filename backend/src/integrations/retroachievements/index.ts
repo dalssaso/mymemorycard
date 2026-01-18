920118c3e15bdd337f46464c7ae724b461d708b5
Henrique Dalssaso <henrique@dalssaso.com.br>
2026-01-18 18:02:11 -0300

    feat(retroachievements): add barrel export

---
+++ backend/src/integrations/retroachievements/index.ts
@@ -0,0 +1,6 @@
+export * from "./retroachievements.types"
+export * from "./retroachievements.service.interface"
+export * from "./retroachievements.service"
+export * from "./retroachievements.controller.interface"
+export * from "./retroachievements.controller"
+export * from "./retroachievements.dto"
