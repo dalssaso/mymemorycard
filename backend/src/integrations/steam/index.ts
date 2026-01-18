7b97bbdeee1ea5b10dab420350e5d5e0932ddeae
Henrique Dalssaso <henrique@dalssaso.com.br>
2026-01-18 17:26:57 -0300

    feat(steam): add barrel export for steam integration

---
+++ backend/src/integrations/steam/index.ts
@@ -0,0 +1,6 @@
+export * from "./steam.types"
+export * from "./steam.service.interface"
+export * from "./steam.service"
+export * from "./steam.controller.interface"
+export * from "./steam.controller"
+export * from "./steam.dto"
